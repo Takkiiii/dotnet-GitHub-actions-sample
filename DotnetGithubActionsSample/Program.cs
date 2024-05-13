84b1bb03a78716bfc93a920f5c5ba03593176bd1
Takkiiii <takizawa@photron.co.jp>
2024-05-13 21:49:02 +0900

    .

--- DotnetGithubActionsSample/Program.cs
+++ DotnetGithubActionsSample/Program.cs
@@ -1,6 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var p = new Person();
-p.Age = 20;
-p.Name = "TestUser";
-Console.WriteLine($"{p.Name} ({p.Age})");
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    foreach(var arg in args){
+                Console.WriteLine(arg);
+            }
+        
+    var p = new Person();
+    p.Age = 20;
+    p.Name = "TestUser";
+    Console.WriteLine($"{p.Name} ({p.Age})");
+  }
+}
+
