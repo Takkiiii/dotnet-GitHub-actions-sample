e52f1222734160947a7e01e68c198067c09408e2
Takkiiii <takizawa@photron.co.jp>
2024-05-13 21:45:56 +0900

    .

--- DotnetGithubActionsSample/Program.cs
+++ DotnetGithubActionsSample/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var p = new Person();
+p.Age = 20;
+p.Name = "TestUser";
+Console.WriteLine($"{p.Name} ({p.Age})");
